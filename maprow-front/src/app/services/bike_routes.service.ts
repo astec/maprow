a5adb033fddffd1ece8f7d59d569c6f44ff9c591
Jakub  Rutkowski <p-jru@astec.net>
2023-07-20 08:34:54 +0200

    Changed the way popups are built

--- maprow-front/src/app/services/bike_routes.service.ts
+++ maprow-front/src/app/services/bike_routes.service.ts
@@ -15,10 +15,13 @@ export class BRouteService {
     this.http.get(this.geo).subscribe((res: any) => {
         L.geoJSON(res, {
             onEachFeature: function (feature, layer) {
-                layer.bindPopup(feature.properties.name);
-            }
+                var popupMessage = feature.properties.name + "<br>" + "Długość w km:" + feature.properties.length_in_km + "<br>" + feature.properties.stroke;
+                layer.bindPopup(popupMessage);
+            },
+            style: function(feature){
+                return {"color":"#999999"};
+            },
          }).addTo(map);
     });
-
   }
 }
